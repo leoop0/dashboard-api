85adb06badaa1d6737ff414e403131d427267f83
leoop0 <72023484+leoop0@users.noreply.github.com>
2021-06-11 19:48:37 +0200

    Chakra ui

--- src/components/Minecraft.jsx
+++ src/components/Minecraft.jsx
@@ -1,41 +1,54 @@
-import React, { useState } from 'react';
-import '../styles/Minecraft.css'
+import React, { useState } from "react";
+import { Heading, Input } from "@chakra-ui/react";
+import { Button, color } from "@chakra-ui/react";
+import "../styles/Minecraft.css";
 const api = {
-    base : 'https://minecraft-api.com/api/skins/'
-  }
+  base: "https://minecraft-api.com/api/skins/",
+};
 
 function Minecraft() {
   let skinBox = document.querySelector("#boxSkin");
-  console.log(skinBox)
-    const [query, setQuery] = useState('');
-  
-    function searchMinecraftPlayer(){
-      let minecraftPlayer = api.base+query;
-      console.log(minecraftPlayer);
-      skinBox.innerHTML= "<div className='skin-result' style='border:solid 1px #21d9c0; border-radius: 10px; padding:20px; max-width: 500px; min-width: 350px;'>Vous pouvez retrouver votre skin ici : <a target='blank' href='"+ minecraftPlayer +"'> Click ! </a></div>"
-    }
+  console.log(skinBox);
+  const [query, setQuery] = useState("");
+
+  function searchMinecraftPlayer() {
+    let minecraftPlayer = api.base + query;
+    console.log(minecraftPlayer);
+    skinBox.innerHTML =
+      "<div className='skin-result' style='border:solid 1px #21d9c0; border-radius: 10px; padding:20px; max-width: 500px; min-width: 350px;'>Vous pouvez retrouver votre skin ici : <a target='blank' href='" +
+      minecraftPlayer +
+      "'> Click ! </a></div>";
+  }
 
   return (
-  <main>
+    <main>
       <div className="minecraft-container">
         <div className="minecraft-search-container search-container">
-        <input 
-        type="text"
-        placeholder="Recherchez un joueur de Minecraft"
-        className="input-minecraft inputSearch"
-        onChange = {e => setQuery(e.target.value)} 
-        value={query}
-        />
-        <button className="submit-minecraft submitSearch" onClick={searchMinecraftPlayer}>Envoyer</button>
+          <Input
+            type="text"
+            placeholder="Recherchez un joueur de Minecraft"
+            className="input-minecraft inputSearch"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            size="md"
+            w="450px"
+            variant="filled"
+          />
+          <Button
+            className="submit-minecraft submitSearch"
+            onClick={searchMinecraftPlayer}
+            ml={2}
+            colorScheme="green"
+          >
+            Envoyer
+          </Button>
         </div>
         <div className="return-skin" id="skin">
-        <div className="skin-box" id="boxSkin">
-
-</div>
+          <div className="skin-box" id="boxSkin"></div>
         </div>
-        </div>
-        <div id="toasts"></div>
-  </main>
+      </div>
+      <div id="toasts"></div>
+    </main>
   );
 }
 
